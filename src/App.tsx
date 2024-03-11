589fc80cf2c4d3e62b3841a9e39ff3ef26443238
JasonYang <yzsenmonster@gmail.com>
2024-03-11 11:28:04 +0800

    fix bug: getUser

--- src/App.tsx
+++ src/App.tsx
@@ -1,102 +1,101 @@
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Buzz from './pages/Buzz';
-import Home from './pages/Home';
-import EditBuzz from './pages/EditBuzz';
-import { ToastContainer, toast } from 'react-toastify';
-import './globals.css';
-import 'react-toastify/dist/ReactToastify.css';
-import { MetaletWalletForBtc, btcConnect } from '@metaid/metaid';
-import { BtcConnector } from '@metaid/metaid/dist/core/connector/btc';
-import { useAtom, useSetAtom } from 'jotai';
-import { btcConnectorAtom, connectedAtom, userInfoAtom } from './store/user';
-import { buzzEntityAtom } from './store/buzz';
-import { errors } from './utils/errors';
-import { isNil } from 'ramda';
-import { checkMetaletInstalled } from './utils/wallet';
-import CreateMetaIDModal from './components/MetaIDFormWrap/CreateMetaIDModal';
-import EditMetaIDModal from './components/MetaIDFormWrap/EditMetaIDModal';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Buzz from "./pages/Buzz";
+import Home from "./pages/Home";
+import EditBuzz from "./pages/EditBuzz";
+import { ToastContainer, toast } from "react-toastify";
+import "./globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import { MetaletWalletForBtc, btcConnect } from "@metaid/metaid";
+import { BtcConnector } from "@metaid/metaid/dist/core/connector/btc";
+import { useAtom, useSetAtom } from "jotai";
+import { btcConnectorAtom, connectedAtom, userInfoAtom } from "./store/user";
+import { buzzEntityAtom } from "./store/buzz";
+import { errors } from "./utils/errors";
+import { isNil } from "ramda";
+import { checkMetaletInstalled } from "./utils/wallet";
+import CreateMetaIDModal from "./components/MetaIDFormWrap/CreateMetaIDModal";
+import EditMetaIDModal from "./components/MetaIDFormWrap/EditMetaIDModal";
 
 function App() {
-  const setConnected = useSetAtom(connectedAtom);
-  const [btcConnector, setBtcConnector] = useAtom(btcConnectorAtom);
-  const setBuzzEntity = useSetAtom(buzzEntityAtom);
-  const setUserInfo = useSetAtom(userInfoAtom);
-  const onWalletConnectStart = async () => {
-    await checkMetaletInstalled();
+	const setConnected = useSetAtom(connectedAtom);
+	const [btcConnector, setBtcConnector] = useAtom(btcConnectorAtom);
+	const setBuzzEntity = useSetAtom(buzzEntityAtom);
+	const setUserInfo = useSetAtom(userInfoAtom);
+	const onWalletConnectStart = async () => {
+		await checkMetaletInstalled();
 
-    const _wallet = await MetaletWalletForBtc.create();
-    if (isNil(_wallet?.address)) {
-      toast.warn(errors.NO_METALET_LOGIN);
-      throw new Error(errors.NO_METALET_LOGIN);
-    }
-    const _btcConnector: BtcConnector = await btcConnect(_wallet);
-    console.log('btc connector', _wallet);
+		const _wallet = await MetaletWalletForBtc.create();
+		if (isNil(_wallet?.address)) {
+			toast.warn(errors.NO_METALET_LOGIN);
+			throw new Error(errors.NO_METALET_LOGIN);
+		}
+		const _btcConnector: BtcConnector = await btcConnect(_wallet);
+		console.log("btc connector", _wallet);
 
-    setBtcConnector(_btcConnector as BtcConnector);
+		setBtcConnector(_btcConnector as BtcConnector);
 
-    // const doc_modal = document.getElementById(
-    //   'create_metaid_modal'
-    // ) as HTMLDialogElement;
-    // doc_modal.showModal();
-    if (!_btcConnector.hasMetaid()) {
-      const doc_modal = document.getElementById(
-        'create_metaid_modal'
-      ) as HTMLDialogElement;
-      doc_modal.showModal();
-    } else {
-      console.log('user now', await _btcConnector.getUser());
-      setUserInfo(await _btcConnector.getUser());
-      setConnected(true);
-      setBuzzEntity(await _btcConnector.use('buzz'));
-      console.log('your btc address: ', _btcConnector.address);
-    }
-  };
+		// const doc_modal = document.getElementById(
+		//   'create_metaid_modal'
+		// ) as HTMLDialogElement;
+		// doc_modal.showModal();
+		if (!_btcConnector.hasMetaid()) {
+			const doc_modal = document.getElementById("create_metaid_modal") as HTMLDialogElement;
+			doc_modal.showModal();
+		} else {
+			const resUser = await _btcConnector.getUser();
+			console.log("user now", resUser);
+			setUserInfo(resUser);
+			setConnected(true);
+			setBuzzEntity(await _btcConnector.use("buzz"));
+			console.log("your btc address: ", _btcConnector.address);
+		}
+	};
 
-  // const handleTest = () => {
-  //   const doc_modal = document.getElementById(
-  //     'create_metaid_modal'
-  //   ) as HTMLDialogElement;
-  //   doc_modal.showModal();
-  // };
+	// const handleTest = () => {
+	//   const doc_modal = document.getElementById(
+	//     'create_metaid_modal'
+	//   ) as HTMLDialogElement;
+	//   doc_modal.showModal();
+	// };
 
-  return (
-    <div className='relative'>
-      <Navbar onWalletConnectStart={onWalletConnectStart} />
+	return (
+		<div className="relative">
+			<Navbar onWalletConnectStart={onWalletConnectStart} />
 
-      <div className='container pt-[100px] bg-[black] text-white h-screen overflow-auto'>
-        {/* <button
+			<div className="container pt-[100px] bg-[black] text-white h-screen overflow-auto">
+				{/* <button
           className='btn btn-active btn-accent text-[blue] absolute top-18 left-2'
           onClick={handleTest}
         >
           Test Button
         </button> */}
-        <Routes>
-          <Route
-            path='/'
-            element={<Home onWalletConnectStart={onWalletConnectStart} />}
-          />
-          <Route path='/buzz/:id' element={<Buzz />} />
-          <Route path='/buzz/:id/edit' element={<EditBuzz />} />
-        </Routes>
-      </div>
-      <ToastContainer
-        position='bottom-right'
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme='light'
-      />
+				<Routes>
+					<Route
+						path="/"
+						element={<Home onWalletConnectStart={onWalletConnectStart} />}
+					/>
+					<Route path="/buzz/:id" element={<Buzz />} />
+					<Route path="/buzz/:id/edit" element={<EditBuzz />} />
+				</Routes>
+			</div>
+			<ToastContainer
+				position="bottom-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="light"
+			/>
 
-      <CreateMetaIDModal btcConnector={btcConnector!} />
-      <EditMetaIDModal btcConnector={btcConnector!} />
-    </div>
-  );
+			<CreateMetaIDModal btcConnector={btcConnector!} />
+			<EditMetaIDModal btcConnector={btcConnector!} />
+		</div>
+	);
 }
 
 export default App;
--- src/components/BuzzList/BuzzCard.tsx
+++ src/components/BuzzList/BuzzCard.tsx
@@ -1,203 +1,196 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import FollowButton from '../Buttons/FollowButton';
-import { Heart, Link as LucideLink } from 'lucide-react';
+import FollowButton from "../Buttons/FollowButton";
+import { Heart, Link as LucideLink } from "lucide-react";
 // import { MessageCircle, Send, } from "lucide-react";
-import { isEmpty, isNil } from 'ramda';
-import cls from 'classnames';
-import dayjs from 'dayjs';
-import { Pin } from '.';
-import { useQueries, useQuery, useQueryClient } from '@tanstack/react-query';
-import { getPinDetailByPid } from '../../api/pin';
-import { btcConnectorAtom } from '../../store/user';
-import { useAtomValue } from 'jotai';
-import CustomAvatar from '../CustomAvatar';
+import { isEmpty, isNil } from "ramda";
+import cls from "classnames";
+import dayjs from "dayjs";
+import { Pin } from ".";
+import { useQueries, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getPinDetailByPid } from "../../api/pin";
+import { btcConnectorAtom } from "../../store/user";
+import { useAtomValue } from "jotai";
+import CustomAvatar from "../CustomAvatar";
 // import { sleep } from '../../utils/time';
-import { toast } from 'react-toastify';
-import { fetchCurrentBuzzLikes } from '../../api/buzz';
+import { toast } from "react-toastify";
+import { fetchCurrentBuzzLikes } from "../../api/buzz";
 
 type IProps = {
-  buzzItem: Pin | undefined;
-  onBuzzDetail?: (txid: string) => void;
-  innerRef?: React.Ref<HTMLDivElement>;
+	buzzItem: Pin | undefined;
+	onBuzzDetail?: (txid: string) => void;
+	innerRef?: React.Ref<HTMLDivElement>;
 };
 
 const BuzzCard = ({ buzzItem, onBuzzDetail, innerRef }: IProps) => {
-  const btcConnector = useAtomValue(btcConnectorAtom);
-  const queryClient = useQueryClient();
+	const btcConnector = useAtomValue(btcConnectorAtom);
+	const queryClient = useQueryClient();
 
-  let summary = buzzItem!.contentSummary;
-  const isSummaryJson = summary.startsWith('{') && summary.endsWith('}');
-  // console.log("isjson", isSummaryJson);
-  // console.log("summary", summary);
-  const parseSummary = isSummaryJson ? JSON.parse(summary) : {};
+	let summary = buzzItem!.contentSummary;
+	const isSummaryJson = summary.startsWith("{") && summary.endsWith("}");
+	// console.log("isjson", isSummaryJson);
+	// console.log("summary", summary);
+	const parseSummary = isSummaryJson ? JSON.parse(summary) : {};
 
-  summary = isSummaryJson ? parseSummary.content : summary;
+	summary = isSummaryJson ? parseSummary.content : summary;
 
-  const attachPids = isSummaryJson
-    ? parseSummary.attachments.map((d: string) => d.split('metafile://')[1])
-    : [];
+	const attachPids = isSummaryJson
+		? parseSummary.attachments.map((d: string) => d.split("metafile://")[1])
+		: [];
 
-  // const attachPids = ["6950f69d7cb83a612fc773d95500a137888f157f1d377cc69c2dd703eebd84eei0"];
-  // console.log("ata", attachPids);
-  // console.log("current address", buzzItem!.address);
+	// const attachPids = ["6950f69d7cb83a612fc773d95500a137888f157f1d377cc69c2dd703eebd84eei0"];
+	// console.log("ata", attachPids);
+	// console.log("current address", buzzItem!.address);
 
-  const { data: currentLikeData } = useQuery({
-    queryKey: ['payLike', buzzItem!.id],
-    queryFn: () => fetchCurrentBuzzLikes(buzzItem!.id),
-  });
-  const isLikeByCurrentUser = (currentLikeData ?? []).find(
-    (d) => d.pinAddress === btcConnector?.address
-  );
+	const { data: currentLikeData } = useQuery({
+		queryKey: ["payLike", buzzItem!.id],
+		queryFn: () => fetchCurrentBuzzLikes(buzzItem!.id),
+	});
+	const isLikeByCurrentUser = (currentLikeData ?? []).find(
+		(d) => d.pinAddress === btcConnector?.address
+	);
 
-  const currentUserInfoData = useQuery({
-    queryKey: ['userInfo', buzzItem!.address],
-    queryFn: () => btcConnector?.getUser(buzzItem!.address),
-  });
-  console.log('currentUserInfoData', currentUserInfoData.data);
-  const attachData = useQueries({
-    queries: attachPids.map((id: string) => {
-      return {
-        queryKey: ['post', id],
-        queryFn: () => getPinDetailByPid({ pid: id }),
-      };
-    }),
-    combine: (results: any) => {
-      return {
-        data: results.map((result: any) => result.data),
-        pending: results.some((result: any) => result.isPending),
-      };
-    },
-  });
-  // console.log("attachData", attachData);
+	const currentUserInfoData = useQuery({
+		queryKey: ["userInfo", buzzItem!.address],
+		queryFn: () => btcConnector?.getUser(buzzItem!.address),
+	});
+	// console.log("address", buzzItem!.address);
+	// console.log("currentUserInfoData", currentUserInfoData.data?.avatar);
+	const attachData = useQueries({
+		queries: attachPids.map((id: string) => {
+			return {
+				queryKey: ["post", id],
+				queryFn: () => getPinDetailByPid({ pid: id }),
+			};
+		}),
+		combine: (results: any) => {
+			return {
+				data: results.map((result: any) => result.data),
+				pending: results.some((result: any) => result.isPending),
+			};
+		},
+	});
+	// console.log("attachData", attachData);
 
-  const handleLike = async (pinId: string) => {
-    if (isLikeByCurrentUser) {
-      toast.warn('You have already liked that buzz...');
-      return;
-    }
+	const handleLike = async (pinId: string) => {
+		if (isLikeByCurrentUser) {
+			toast.warn("You have already liked that buzz...");
+			return;
+		}
 
-    const likeEntity = await btcConnector!.use('like');
-    try {
-      const likeRes = await likeEntity.create({
-        options: [
-          {
-            body: JSON.stringify({ isLike: '1', likeTo: pinId }),
-          },
-        ],
-        noBroadcast: 'no',
-      });
-      console.log('likeRes', likeRes);
-      if (!isNil(likeRes?.revealTxIds[0])) {
-        queryClient.invalidateQueries({ queryKey: ['buzzes'] });
-        queryClient.invalidateQueries({ queryKey: ['payLike', buzzItem!.id] });
-        // await sleep(5000);
-        toast.success('like buzz successfully');
-      }
-    } catch (error) {
-      console.log('error', error);
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      toast.warn((error as any)?.message ?? 'too-long-mempool-chain');
-    }
-  };
+		const likeEntity = await btcConnector!.use("like");
+		try {
+			const likeRes = await likeEntity.create({
+				options: [
+					{
+						body: JSON.stringify({ isLike: "1", likeTo: pinId }),
+					},
+				],
+				noBroadcast: "no",
+			});
+			console.log("likeRes", likeRes);
+			if (!isNil(likeRes?.revealTxIds[0])) {
+				queryClient.invalidateQueries({ queryKey: ["buzzes"] });
+				queryClient.invalidateQueries({ queryKey: ["payLike", buzzItem!.id] });
+				// await sleep(5000);
+				toast.success("like buzz successfully");
+			}
+		} catch (error) {
+			console.log("error", error);
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			toast.warn((error as any)?.message ?? "too-long-mempool-chain");
+		}
+	};
 
-  const renderImages = (pinIds: string[]) => {
-    return (
-      <div className='grid grid-cols-3 gap-2 place-items-center'>
-        {pinIds.map((pinId) => {
-          return (
-            <img
-              className='image'
-              height={'48px'}
-              width={'auto'}
-              src={`https://man-test.metaid.io/content/${pinId}`}
-              alt=''
-              key={pinId}
-            />
-          );
-        })}
-      </div>
-    );
-  };
-  if (isNil(buzzItem)) {
-    return <div>can't fetch this buzz</div>;
-  }
+	const renderImages = (pinIds: string[]) => {
+		return (
+			<div className="grid grid-cols-3 gap-2 place-items-center">
+				{pinIds.map((pinId) => {
+					return (
+						<img
+							className="image"
+							height={"48px"}
+							width={"auto"}
+							src={`https://man-test.metaid.io/content/${pinId}`}
+							alt=""
+							key={pinId}
+						/>
+					);
+				})}
+			</div>
+		);
+	};
+	if (isNil(buzzItem)) {
+		return <div>can't fetch this buzz</div>;
+	}
 
-  return (
-    <div
-      className='w-full border border-white rounded-xl flex flex-col gap-4'
-      ref={innerRef}
-    >
-      <div className='flex items-center justify-between pt-4 px-4'>
-        <div className='flex gap-2 items-center'>
-          {/* <img
+	return (
+		<div className="w-full border border-white rounded-xl flex flex-col gap-4" ref={innerRef}>
+			<div className="flex items-center justify-between pt-4 px-4">
+				<div className="flex gap-2 items-center">
+					{/* <img
 						src={`https://picsum.photos/seed/${imgSeed}/200`}
 						alt="user avatar"
 						className="rounded-full"
 						width={40}
 						height={40}
 					/> */}
-          {isNil(currentUserInfoData.data) ? (
-            <div className='avatar placeholder'>
-              <div className='bg-[#2B3440] text-[#D7DDE4] rounded-full w-12'>
-                <span>{buzzItem!.address.slice(-4, -2)}</span>
-              </div>
-            </div>
-          ) : (
-            <CustomAvatar userInfo={currentUserInfoData.data} />
-          )}
+					{isNil(currentUserInfoData.data) ? (
+						<div className="avatar placeholder">
+							<div className="bg-[#2B3440] text-[#D7DDE4] rounded-full w-12">
+								<span>{buzzItem!.address.slice(-4, -2)}</span>
+							</div>
+						</div>
+					) : (
+						<CustomAvatar userInfo={currentUserInfoData.data} />
+					)}
 
-          <div className='text-gray'>
-            {isNil(currentUserInfoData?.data?.name) ||
-            isEmpty(currentUserInfoData?.data?.name)
-              ? 'metaid-user-' + buzzItem.address.slice(-4)
-              : currentUserInfoData?.data?.name}
-          </div>
-        </div>
-        <FollowButton isFollowed={true} />
-      </div>
-      <div
-        className={cls('border-y border-white p-4', {
-          'cursor-pointer': !isNil(onBuzzDetail),
-        })}
-        onClick={() => onBuzzDetail && onBuzzDetail(buzzItem.id)}
-      >
-        <div className='flex flex-col gap-2'>
-          <div>{summary} </div>
-          {!attachData.pending &&
-            !isEmpty((attachData?.data ?? []).filter((d: any) => !isNil(d))) &&
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            renderImages(attachPids)}
-        </div>
-        <div className='flex justify-between text-gray mt-2'>
-          <div className='flex gap-2 items-center'>
-            <LucideLink size={12} />
-            <div>{buzzItem.rootTxId.slice(0, 8) + '...'}</div>
-          </div>
-          <div>
-            {dayjs.unix(buzzItem.timestamp).format('YYYY-MM-DD HH:mm:ss')}
-          </div>
-        </div>
-      </div>
+					<div className="text-gray">
+						{isNil(currentUserInfoData?.data?.name) ||
+						isEmpty(currentUserInfoData?.data?.name)
+							? "metaid-user-" + buzzItem.address.slice(-4)
+							: currentUserInfoData?.data?.name}
+					</div>
+				</div>
+				<FollowButton isFollowed={true} />
+			</div>
+			<div
+				className={cls("border-y border-white p-4", {
+					"cursor-pointer": !isNil(onBuzzDetail),
+				})}
+				onClick={() => onBuzzDetail && onBuzzDetail(buzzItem.id)}
+			>
+				<div className="flex flex-col gap-2">
+					<div>{summary} </div>
+					{!attachData.pending &&
+						!isEmpty((attachData?.data ?? []).filter((d: any) => !isNil(d))) &&
+						// eslint-disable-next-line @typescript-eslint/no-explicit-any
+						renderImages(attachPids)}
+				</div>
+				<div className="flex justify-between text-gray mt-2">
+					<div className="flex gap-2 items-center">
+						<LucideLink size={12} />
+						<div>{buzzItem.rootTxId.slice(0, 8) + "..."}</div>
+					</div>
+					<div>{dayjs.unix(buzzItem.timestamp).format("YYYY-MM-DD HH:mm:ss")}</div>
+				</div>
+			</div>
 
-      <div className='flex items-center justify-between pb-4 px-4'>
-        <div className='flex gap-2'>
-          <Heart
-            className={cls(
-              { 'text-[red]': isLikeByCurrentUser },
-              'cursor-pointer'
-            )}
-            fill={isLikeByCurrentUser && 'red'}
-            onClick={() => handleLike(buzzItem!.id)}
-          />
+			<div className="flex items-center justify-between pb-4 px-4">
+				<div className="flex gap-2">
+					<Heart
+						className={cls({ "text-[red]": isLikeByCurrentUser }, "cursor-pointer")}
+						fill={isLikeByCurrentUser && "red"}
+						onClick={() => handleLike(buzzItem!.id)}
+					/>
 
-          {!isNil(currentLikeData) ? currentLikeData.length : null}
-          {/* <MessageCircle />
+					{!isNil(currentLikeData) ? currentLikeData.length : null}
+					{/* <MessageCircle />
 					<Send /> */}
-        </div>
-        <div className='btn btn-sm rounded-full'>Want To Buy</div>
-      </div>
-    </div>
-  );
+				</div>
+				<div className="btn btn-sm rounded-full">Want To Buy</div>
+			</div>
+		</div>
+	);
 };
 
 export default BuzzCard;
