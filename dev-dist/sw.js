e11959f41371a171623f6aae4af2a545ac94984b
jasonyang <yzsenmonster@gmail.com>
2024-03-28 09:41:56 +0800

    WIP

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.smoqtshub2"
+    "revision": "0.0q02282tf1o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,43 @@
-import BuzzList from "../components/BuzzList";
+import BuzzList from '../components/BuzzList';
 // import RecommendUsers from "../components/RecommendUsers";
-import { connectedAtom } from "../store/user";
-import { useAtomValue } from "jotai";
+import { connectedAtom } from '../store/user';
+import { useAtomValue } from 'jotai';
 type IProps = {
-	onWalletConnectStart: () => Promise<void>;
+  onWalletConnectStart: () => Promise<void>;
 };
 
 const Home = ({ onWalletConnectStart }: IProps) => {
-	const connected = useAtomValue(connectedAtom);
+  const connected = useAtomValue(connectedAtom);
 
-	if (connected) {
-		return (
-			<main className="">
-				{/* <RecommendUsers /> */}
-				<BuzzList />
-			</main>
-		);
-	}
-	return (
-		<main className="h-full place-content-center overflow-hidden">
-			<img
-				src="/orbuzz_home_img.png"
-				width={650}
-				height={600}
-				alt="Picture of the home"
-				className="absolute top-[10rem]"
-			/>
-			<div className="flex flex-col items-center mt-[2rem]">
-				<div className="text-main font-['impact'] text-[120px]">BIT DID</div>
-				<div className="text-[white]">Claim your DID on bitcoin</div>
-				<div
-					className="btn btn-primary rounded-full mt-[8rem] text-[20px] font-medium	w-[220px]"
-					onClick={onWalletConnectStart}
-				>
-					Connect Wallet
-				</div>
-			</div>
-		</main>
-	);
+  if (connected) {
+    return (
+      <main className=''>
+        {/* <RecommendUsers /> */}
+        <BuzzList />
+      </main>
+    );
+  }
+  return (
+    <main className='h-full place-content-center overflow-hidden'>
+      <img
+        src='/orbuzz_home_img.png'
+        width={650}
+        height={600}
+        alt='Picture of the home'
+        className='absolute top-[10rem]'
+      />
+      <div className='flex flex-col items-center mt-[12rem]'>
+        <div className="text-main font-['impact'] text-[120px]">BIT DID</div>
+        <div className='text-[white]'>Claim your DID on bitcoin</div>
+        <div
+          className='btn btn-primary rounded-full mt-[8rem] text-[20px] font-medium	w-[220px]'
+          onClick={onWalletConnectStart}
+        >
+          Connect Wallet
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
