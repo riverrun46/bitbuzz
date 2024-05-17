265a4e194fc5fb74f56f28c8e8a2576025e811d3
JasonYang <yzsenmonster@gmail.com>
2024-05-17 09:49:17 +0800

    add metaid field

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,21 +1,22 @@
-import { type BtcConnector } from '@metaid/metaid/dist/core/connector/btc';
-import { MetaIDWalletForBtc } from '@metaid/metaid/dist/wallets/metalet/btcWallet';
-import { atom } from 'jotai';
-import { isNil } from 'ramda';
-import { BtcNetwork } from '../api/request';
+import { type BtcConnector } from "@metaid/metaid/dist/core/connector/btc";
+import { MetaIDWalletForBtc } from "@metaid/metaid/dist/wallets/metalet/btcWallet";
+import { atom } from "jotai";
+import { isNil } from "ramda";
+import { BtcNetwork } from "../api/request";
 
 export type UserInfo = {
-  number: number;
-  rootTxId: string;
-  name: string;
-  nameId: string;
-  avatarId: string;
-  bioId: string;
-  address: string;
-  avatar: string | null;
-  bio: string;
-  soulbondToken: string;
-  unconfirmed: string;
+	number: number;
+	rootTxId: string;
+	name: string;
+	nameId: string;
+	avatarId: string;
+	bioId: string;
+	address: string;
+	avatar: string | null;
+	bio: string;
+	soulbondToken: string;
+	unconfirmed: string;
+	metaid: string;
 };
 
 export const connectedAtom = atom(false);
@@ -23,10 +24,8 @@ export const btcConnectorAtom = atom<BtcConnector | null>(null);
 export const userInfoAtom = atom<UserInfo | null>(null);
 
 export const initStillPoolAtom = atom<boolean>((get) => {
-  const userInfo = get(userInfoAtom);
-  return isNil(userInfo)
-    ? false
-    : userInfo.unconfirmed.split(',').includes('number');
+	const userInfo = get(userInfoAtom);
+	return isNil(userInfo) ? false : userInfo.unconfirmed.split(",").includes("number");
 });
 
 export const walletAtom = atom<MetaIDWalletForBtc | null>(null);
@@ -37,12 +36,12 @@ export const walletAtom = atom<MetaIDWalletForBtc | null>(null);
  */
 export const unisatInstalledAtom = atom(false);
 export const accountsAtom = atom<string[]>([]);
-export const publicKeyAtom = atom('');
-export const addressAtom = atom('');
+export const publicKeyAtom = atom("");
+export const addressAtom = atom("");
 export const balanceAtom = atom({
-  confirmed: 0,
-  unconfirmed: 0,
-  total: 0,
+	confirmed: 0,
+	unconfirmed: 0,
+	total: 0,
 });
-export const networkAtom = atom<BtcNetwork>('testnet');
-export const globalFeeRateAtom = atom<string>('1200');
+export const networkAtom = atom<BtcNetwork>("testnet");
+export const globalFeeRateAtom = atom<string>("60");
