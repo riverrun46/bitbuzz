6fa2eb74ea09d3fb6af51e88e2250ea8fe0edc5f
JasonYang <yzsenmonster@gmail.com>
2024-05-21 18:44:55 +0800

    update metaid

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.o98cfsh4vgo"
+    "revision": "0.sq9rpnmk5d"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
