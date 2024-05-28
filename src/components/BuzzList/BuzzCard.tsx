dd661ffc99eccec75bd842275dd4bc7cc2a4972d
JasonYang <yzsenmonster@gmail.com>
2024-05-28 23:00:39 +0800

    fix error

--- src/components/BuzzList/BuzzCard.tsx
+++ src/components/BuzzList/BuzzCard.tsx
@@ -488,22 +488,20 @@ const BuzzCard = ({ buzzItem, onBuzzDetail, innerRef }: IProps) => {
               </div>
             </div>
           </div>
-          {!isEmpty(btcConnector?.metaid ?? '') &&
-            !isEmpty(metaid ?? '') &&
-            btcConnector?.metaid !== metaid && (
-              <FollowButton
-                isFollowed={(myFollowingListData?.list ?? []).includes(metaid)}
-                isFollowingPending={
-                  (myFollowingList ?? []).includes(metaid ?? '') &&
-                  !(myFollowingListData?.list ?? []).includes(metaid)
-                }
-                isUnfollowingPending={
-                  !(myFollowingList ?? []).includes(metaid ?? '') &&
-                  (myFollowingListData?.list ?? []).includes(metaid)
-                }
-                handleFollow={handleFollow}
-              />
-            )}
+          {btcConnector?.metaid !== metaid && (
+            <FollowButton
+              isFollowed={(myFollowingListData?.list ?? []).includes(metaid)}
+              isFollowingPending={
+                (myFollowingList ?? []).includes(metaid ?? '') &&
+                !(myFollowingListData?.list ?? []).includes(metaid)
+              }
+              isUnfollowingPending={
+                !(myFollowingList ?? []).includes(metaid ?? '') &&
+                (myFollowingListData?.list ?? []).includes(metaid)
+              }
+              handleFollow={handleFollow}
+            />
+          )}
         </div>
         <div
           className={cls('border-y border-white p-4', {
