8791d996b65b921aa5d3ebaa1db78aecd9366c4c
JasonYang <yzsenmonster@gmail.com>
2024-07-28 17:03:48 +0800

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import wasm from 'vite-plugin-wasm';
 // import { resolve } from 'path';
-import { environment } from './src/utils/environments';
 
 // https://vitejs.dev/config/
 export default defineConfig({
